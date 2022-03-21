cd92520fad12e89ba4bd03fa944d88186d4eab70
Abriele Qudsi <dev.qudsi@gmail.com>
2022-03-21 21:54:52 +0000

    feat(be): implement IDE page

---
+++ client/src/pages/IDE.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+// Other
+import Prompt from "../components/IDE/Prompt";
+import CodeEditor from "../components/IDE/CodeEditor";
+// import TestCase from "../components/IDE/TestCase";
+import { useParams } from "react-router-dom";
+
+function IDE({ isLight }) {
+  const { number } = useParams();
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12} md={6}>
+        <Prompt problemNumber={number} />
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <CodeEditor problemNumber={number} isLight={isLight} />
+      </Grid>
+      {/* <Grid item xs={12} md={6}>
+        <TestCase problemNumber={number} />
+      </Grid> */}
+    </Grid>
+  );
+}
+
+export default IDE;
