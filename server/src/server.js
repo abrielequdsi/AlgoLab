783d0ad644729a6cf4fa29532dbf898d5efdf097
Abriele Qudsi <dev.qudsi@gmail.com>
2022-03-22 10:35:11 +0000

    feat(be): add db sync

---
+++ server/src/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const db = require("./models");
+
+const app = express();
+const corsOptions = {
+    origin: "http://localhost:8000",
+};
+
+const PORT = process.env.PORT || 8000;
+
+app.use(cors());
+app.use(express.json());
+
+(async function () {
+    try {
+        await db.sequelize.sync(); // Connect db & Create db
+        app.listen(PORT, () => {
+            console.log(`🚀 Server is listening at http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.log("❌❌❌ DATABASE FAILED TO CONNECT");
+    }
+})();
