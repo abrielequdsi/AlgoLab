4236bbdc08070a1bdf89500004bb2e5d339a764d
Abriele Qudsi <dev.qudsi@gmail.com>
2022-03-22 10:36:18 +0000

    feat(be): implement user and role model

---
+++ server/src/models/index.js
@@ -0,0 +1,30 @@
+const config = require("../configs/db.config.js");
+const Sequelize = require("sequelize");
+
+// Connecting to database
+const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
+    host: config.HOST,
+    dialect: config.dialect,
+});
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.user = require("../models/user.model.js")(sequelize, Sequelize);
+db.role = require("../models/role.model.js")(sequelize, Sequelize);
+db.role.belongsToMany(db.user, {
+    through: "user_roles",
+    foreignKey: "roleId",
+    otherKey: "userId",
+});
+db.user.belongsToMany(db.role, {
+    through: "user_roles",
+    foreignKey: "userId",
+    otherKey: "roleId",
+});
+db.ROLES = ["user", "admin", "moderator"];
+
+module.exports = db;
+
+// https://www.bezkoder.com/node-js-jwt-authentication-mysql/
---
+++ server/src/models/user.model.js
@@ -0,0 +1,9 @@
+module.exports = (sequelize, Sequelize) => {
+    const User = sequelize.define("users", {
+        username: { type: Sequelize.STRING },
+        email: { type: Sequelize.STRING },
+        password: { type: Sequelize.STRING },
+        solvedQuestion: { type: Sequelize.JSON },
+    });
+    return User;
+};
